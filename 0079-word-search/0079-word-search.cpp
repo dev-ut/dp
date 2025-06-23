d9762302280436ece53b54b404e723222be94d6b
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-06-23 12:02:37 +0530

    Time: 806 ms (32.63%), Space: 11.5 MB (47.42%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board, int row, int col, string& word, int idx, bool& flag) 
+void sfound(vector<vector<char>>& board, string word,int windx,int r,int c,bool &flag)
+{
+    if(windx==word.length())
     {
-        if (idx == word.size()) 
-        {
-            flag = true;
+        flag = true;
             return;
-        }
-
-        int n = board.size();
-        int m = board[0].size();
-
-        // Out of bounds or mismatch
-        if (row < 0 || col < 0 || row >= n || col >= m || board[row][col] != word[idx])
-            return;
-
-        // Mark as visited
-        char temp = board[row][col];
-        board[row][col] = '#';
-
-        // Move in all 4 directions
-
-        // Right
-        solve(board, row, col + 1, word, idx + 1, flag);
-
-        // Down
-        solve(board, row + 1, col, word, idx + 1, flag);
-
-        // Left
-        solve(board, row, col - 1, word, idx + 1, flag);
-
-        // Up
-        solve(board, row - 1, col, word, idx + 1, flag);
-
-        // Backtrack
-        board[row][col] = temp;
     }
 
-    bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (board[i][j] == word[0]) {
-                    bool flag = false;
-                    solve(board, i, j, word, 0, flag);
-                    if (flag) return true;
+    if(c<0 ||c>=board[0].size()||r<0||r>=board.size()||board[r][c]!= word[windx]) return ;
+
+     // Mark as visited
+        char temp = board[r][c];
+        board[r][c] = '.';
+
+    // moving in right direction 
+    sfound(board,word,windx+1,r,c+1,flag);
+    // moving in down direction 
+    sfound(board,word,windx+1,r+1,c,flag);
+     // moving in left direction 
+    sfound(board,word,windx+1,r,c-1,flag);
+     // moving inup direction 
+    sfound(board,word,windx+1,r-1,c,flag);
+
+    // mark it unvisted and rstore the element
+    board[r][c] = temp;  /// backtrack
+
+
+
+
+}
+    bool exist(vector<vector<char>>& board, string word) 
+    {
+        int n=board.size();
+        int m=board[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(board[i][j]==word[0]) // mtlb ki jaise hi first leete mile ap search krna chlu kro in all possible directuos
+                {
+                    bool flag=false;
+                    sfound(board,word,0,i,j,flag);
+                    if(flag==true) return true;
                 }
             }
         }
