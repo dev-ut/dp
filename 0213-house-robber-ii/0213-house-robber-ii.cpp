695dfaf0e0ba098fa917637d57e32b3b0dd98a82
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-08-04 15:58:03 +0530

    Time: 0 ms (100%), Space: 10.9 MB (39.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-int solve(vector<int>& nums, int i, int n, vector<int> &dp) 
+int solve(vector<int>& nums, int indx, int n,vector<int>&dp) 
 {
-        if (i > n) return 0;
+        if (indx > n) return 0;
         // Check if the result for the current index is already computed
-        if (dp[i] != -1) return dp[i];
+        if(dp[indx]!=-1)return dp[indx];
         
         // Option 1: Rob the current house and move to the house after the next one
-        int robCurrent = nums[i] + solve(nums, i + 2, n,dp);
+        int robCurrent = nums[indx] + solve(nums, indx + 2, n,dp);
         
         // Option 2: Skip the current house and move to the next one
-        int skipCurrent = solve(nums, i + 1, n,dp);
+        int skipCurrent = solve(nums, indx+ 1,n,dp);
         
         // Return the maximum of both options
-        return dp[i]= max(robCurrent, skipCurrent);
+        return dp[indx]= max(robCurrent, skipCurrent);
     }
     int rob(vector<int>& nums) 
     {
@@ -22,18 +22,13 @@ int solve(vector<int>& nums, int i, int n, vector<int> &dp)
         if(n==1) return nums[0];
 
         if(n==2) return max(nums[0],nums[1]);
+        // when we are not taking the last elemnt as we are taking the first elemnt coz ther aye adjacent  
+        vector<int>dp1(n+1,-1);
+        int case1= solve(nums,0,n-2,dp1);
+        //we are not taking the first elemnt as we are taking the last elment 
+        vector<int>dp2(n+1,-1); 
+        int case2= solve(nums,1,n-1,dp2);
 
-        vector<int> dp1(n,-1);  // n for 0  based index
-        //taking oth index house
-        int case1= solve(nums,0,n-2,dp1);  // excluding n-1th elemt or last elemnt.
-
-        vector<int>dp2(n+1,-1);  // n+1 for 1 based index
-
-        //taking  1th index we can include the last elemnt
-        int case2=solve(nums,1,n-1,dp2);
-
-        return max(case1,case2); 
-
-
+        return max(case1,case2);
     }
 };
