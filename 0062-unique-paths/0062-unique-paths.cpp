8b60732e3c23318aedc8c061db964aea9a8cd63d
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-08-04 18:15:36 +0530

    Time: 0 ms (100%), Space: 7.9 MB (87.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) 
-{
-    // Tabulation
-    int dp[m][n];
-
-    for(int i = 0; i < m; i++)
     {
-        for(int j = 0; j < n; j++)
+        int dp[m][n];
+
+        for(int i=0;i<m;i++)
         {
-            if(i == 0 && j == 0)  // starting cell
-            {
-                dp[i][j] = 1;
-            }
-            else if(i == 0)
-            {
-                dp[i][j] = dp[i][j - 1]; // pehli row
-            }
-            else if(j == 0)
-            {
-                dp[i][j] = dp[i - 1][j]; // pehla column
-            }
+          for(int j=0;j<n;j++)
+          {
+            if(i==0 || j==0) dp[i][j]=1;
+
             else
             {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]; // up + left
+                dp[i][j]=dp[i-1][j]+ dp[i][j-1];
             }
+          }
         }
+        return dp[m-1][n-1];
+        
     }
-
-    return dp[m - 1][n - 1];
-}
-
 };
