12e8e3142dcdcfdc578b260c9e64b00446f3d0dd
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-08-16 12:39:31 +0530

    Time: 0 ms (100%), Space: 17.9 MB (54.53%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -2,37 +2,45 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) 
     {
-      int n = nums.size();
-    if (n == 0) return false;
-    int lo = 0;
-    int hi = n - 1;
+        int n = nums.size();
+        if (n == 0) return false;
+        int lo = 0;
+        int hi = n - 1;
 
-    while (lo <= hi) {
-        int mid = lo + (hi - lo) / 2;
-        if (nums[mid] == target) return true;
+        while (lo <= hi) 
+        {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return true;
 
-        if (nums[lo] == nums[mid] && nums[mid] == nums[hi]) 
-        { // Handling duplicates   // this is extra chhez  nums = [1,0,1,1,1], target = 0
-            lo++;
-            hi--;
-        } 
-        else if (nums[lo] <= nums[mid]) 
-        { // Left part is sorted
-            if (nums[lo] <= target && target < nums[mid]) { // Target is in the left part
-                hi = mid - 1;
-            } else 
+            // Handle duplicates
+            if (nums[lo] == nums[mid] && nums[mid] == nums[hi]) 
             {
-                lo = mid + 1; // Target is in the right part
-            }
-        } else { // Right part is sorted
-            if (nums[mid] < target && target <= nums[hi]) { // Target is in the right part
-                lo = mid + 1;
-            } else {
-                hi = mid - 1; // Target is in the left part
+                lo++;
+                hi--;
+            } 
+            else if (nums[mid] <= nums[hi])   // ✅ Right part sorted
+            {   
+                if (nums[mid] < target && target <= nums[hi]) 
+                {   // Target right me hai
+                    lo = mid + 1;
+                } 
+                else 
+                {   // Target left me hoga
+                    hi = mid - 1;
+                }
+            } 
+            else    // ✅ Left part sorted
+            {   
+                if (nums[lo] <= target && target < nums[mid]) 
+                {   // Target left me hai
+                    hi = mid - 1;
+                } 
+                else 
+                {   // Target right me hoga
+                    lo = mid + 1; 
+                }
             }
         }
-    }
-
-    return false; // Target not found
+        return false; 
     }
 };
