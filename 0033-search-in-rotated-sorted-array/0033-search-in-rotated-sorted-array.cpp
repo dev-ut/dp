65a03550bb9e1dde56a0b650cc3295e186cb0843
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-08-16 12:05:07 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,32 +2,41 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) 
     {
-        int n = nums.size();
-    if (n == 0) return -1;
-    int lo = 0;
-    int hi = n - 1;
+        int n=nums.size();
 
-    while (lo <= hi) {
-        int mid = lo + (hi - lo) / 2;
-        if (nums[mid] == target) return mid;
+        int lo=0;
+        int hi=n-1;
 
-        if (nums[lo] == target) return lo;  // Check if lo is the target
-        
-        if (nums[lo] <= nums[mid]) { // it means it's a left sorted part
-            if (nums[lo] <= target && target < nums[mid]) { // if so, the value lies in the left part only
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
+        while(lo<=hi)
+        {
+            // we will serch for sorted parts  // suppose right sorted
+
+            int mid=lo+(hi-lo)/2;
+            if(nums[mid]==target) return mid;
+
+            if(nums[mid]<nums[hi])  // right sorted
+            { 
+                if(nums[mid]<=target && nums[hi]>=target)  // yes the target ies here
+                {
+                    lo=mid+1;
+                }
+                else
+                {
+                    hi=mid-1;
+                }
             }
-        } else {
-            if (nums[mid] < target && target <= nums[hi]) { // it means it lies in the right sorted part
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
+            else   // left soretd
+            {
+               if(nums[mid]>=target && nums[lo]<=target)  // ye yhi milega 4,5,6,7,0,1,2  // target=6
+               {
+                hi=mid-1;
+               }
+               else
+               {
+                lo=mid+1;
+               }
             }
         }
-    }
-
-    return -1;
+        return -1;
     }
 };
