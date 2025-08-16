a2249c20f934cf5317f81ef466e18d8712ae64e4
Utkarsh Tiwari <utkarsht701@gmail.com>
2025-08-16 15:34:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        int n=nums.size();
+
+        int lo=0;
+        int hi=n-1;
+        while(lo<=hi)
+        {
+           int mid=lo+(hi-lo)/2;
+           if(mid==0)// ye tb hoga jb sorted in descending order
+           {
+             if(nums[mid]>nums[mid+1])
+              {
+                return mid;
+              }
+              else
+              {
+                lo=mid+1;
+              }
+           }
+
+           if(mid==n-1)  // ye kab ayega when u ll haveelment sorted in ascendommg order
+           {
+              if(nums[mid]>nums[mid-1])
+              {
+                return mid;
+              }
+              else
+              {
+                hi=mid-1;
+              }
+           }
+          else if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1])
+           {
+            return mid;
+           }
+           else if(nums[mid]<nums[mid+1])
+           {
+            lo=mid+1;
+           }
+           else
+           {
+            hi=mid-1;
+           }
+        }
+        return -1;
+    }
+};
