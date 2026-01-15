69adfbfce3f4186547f451c1986b1245ef571648
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-01-15 12:58:46 +0530

    Time: 2 ms (39.91%), Space: 9.9 MB (60.83%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
-    // Recursive function to generate subsets
-    void subset(vector<int>& nums, vector<int> ans, int index, vector<vector<int>>& fans) {
-        // Base Case: If we have processed all elements, add the current output to the answer
-        if (index >= nums.size()) 
-        {
-            fans.push_back(ans);
-            return;
-        }
-              ans.push_back(nums[index]);
-             subset(nums,ans,index+1,fans);
-             ans.pop_back();  // backtrack
-             //notake    // iska caase pehel likhoge to  u dont have to write pop back 
-               subset(nums,ans,index+1,fans);
-
-              //take
-              
-
-            //  ans.pop_back(); // backtrack
-             
-
+void subset(vector<int>& nums,vector<vector<int>> &fans, vector<int>&ans,int indx)
+{
+    if(indx>=nums.size())
+    {
+     fans.push_back(ans);
+     return ;
     }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> fans; // Final output storage
-        vector<int> ans;     // Temporary output storage
-        int index = 0;
-        subset(nums, ans, index, fans); // Start generating subsets
+
+    // take case
+    ans.push_back(nums[indx]);
+    subset(nums,fans,ans,indx+1);
+    // backtrack after we reach the base case 
+    ans.pop_back();
+    // not take skip current indx
+    subset(nums,fans,ans,indx+1);
+}
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector<vector<int>> fans;
+        vector<int>ans;
+        subset(nums,fans,ans,0);
         return fans;
     }
 };
