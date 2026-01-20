267d4140454f943c91965c7980d03ca657c30c71
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-01-20 20:37:08 +0530

    Time: 0 ms (100%), Space: 16.2 MB (93.68%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,35 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* rotateRight(ListNode* head, int k)
+    ListNode* rotateRight(ListNode* head, int k) 
     {
-        if(head==NULL || head->next==NULL) return head;  // edge xase 2
-        ListNode* temp=head;
-        ListNode *tail=NULL;
-        int size=0;
-        //get the size of the list  and tail ko end main kr do taki excahnge kr ske
+       if(k==0 || head==NULL || head->next==NULL) return head;
+       ListNode * tail=head;
+       ListNode* temp=head;
+       int size=0;
+       while(temp!=NULL)
+       {
+        if(temp->next!=NULL) tail=tail->next;
+        temp=temp->next;
+        size++;
+       }
+       k=k%size;
+       if(k==0) return head;
+       temp=head;
+       // amke it cyclic 
+       tail->next=head;
+       int it=size-k;
+       while(it>1)
+       {
+        temp=temp->next;
+        it--;
+       }
+       // connct 
+       ListNode * nwhead=temp->next;
+       temp->next=nullptr;
 
-        while(temp!=NULL)
-        {
-            if(temp->next!=NULL) tail=temp->next;   // tail bhi set hogay
-           temp=temp->next;
-           size++;
-        }
+       return nwhead;
 
-        // temp ko fr se head pr le aao
-         k=k%size;   // if(k>n) make it small
-        if(k==0) return head;   // edge case 1  that is if k=25 in size of 5 :25%5=0 ie return list
-        temp=head;
-
-        for(int i=1;i<size-k;i++)
-        {
-            temp=temp->next;
-        }
-        tail->next=head;
-        head=temp->next;
-        temp->next=NULL;
-         
-         return head;
-        
     }
 };
