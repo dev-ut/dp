6bed15ad730b856efd14dd17ac29b298e1f1fb2b
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-01-20 14:49:25 +0530

    Time: 0 ms (100%), Space: 15 MB (58.99%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -12,43 +12,33 @@ class Solution {
 public:
 ListNode* removeNthFromEnd(ListNode* head, int n) 
 {
-    // Agar linked list khaali hai
-    if (head == NULL) return NULL;
+   int size=0;
+   ListNode *temp=head;
+   if(head==NULL) return NULL;
+   while(temp!=NULL)
+   {
+    size++;
+    temp=temp->next;
+   } 
 
-    int size = 0; // Linked list ki size ko track karne ke liye
-    ListNode* temp = head;
+   // if agr head hi remove krna pda to 
+   if(n==size)
+   {
+    ListNode* newhead=head->next;
+    delete head;
+    return newhead;
+   }
+   int it=size-n;
+   temp=head;
+   for(int i=0;i<it-1;i++)
+   {
+    temp=temp->next;
+   }
+   ListNode* del=temp->next;
+   temp->next=temp->next->next;
+   delete del;
 
-    // Linked list ki length calculate karte hain
-    while (temp != NULL)
-    {
-        temp = temp->next;
-        size++;
-    }
-
-    // Agar n == size, toh head ko remove karna hai
-    if (n == size) 
-    {
-        ListNode* newHead = head->next; // Naya head set karte hain
-        delete head; // Old head ko delete karte hain
-        return newHead; // Naya head return karte hain
-    }
-    
-    int m = size - n; // Remove karne ke liye node tak pahuchne ke liye
-
-    temp = head;
-    // Pahle se m-1 nodes tak pahuchne ke liye
-    while (m > 1)
-    {
-        temp = temp->next;
-        m--;
-    }
-
-    // n-th node ko remove karte hain
-    ListNode* nodeToDelete = temp->next; // Node jo remove karna hai
-    temp->next = temp->next->next; // Link ko update karte hain
-    delete nodeToDelete; // Node ko delete karte hain
-
-    return head; // Updated head return karte hain
+   return head; 
 }
 
 };
