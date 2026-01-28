a7522e67b0c544b39c64f1f50bebba681e182aa1
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-01-28 17:09:31 +0530

    Time: 0 ms (100%), Space: 14.9 MB (91.23%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -12,33 +12,62 @@ class Solution {
 public:
 ListNode* removeNthFromEnd(ListNode* head, int n) 
 {
-   int size=0;
-   ListNode *temp=head;
-   if(head==NULL) return NULL;
-   while(temp!=NULL)
-   {
-    size++;
-    temp=temp->next;
-   } 
 
-   // if agr head hi remove krna pda to 
-   if(n==size)
-   {
-    ListNode* newhead=head->next;
-    delete head;
-    return newhead;
-   }
-   int it=size-n;
-   temp=head;
-   for(int i=0;i<it-1;i++)
-   {
-    temp=temp->next;
-   }
-   ListNode* del=temp->next;
-   temp->next=temp->next->next;
-   delete del;
+    int size=0;
+    ListNode* temp=head;
+    while(temp!=NULL)
+    {
+        size++;
+        temp=temp->next;
+    }
+    temp=head;
+    // if head node hi delet krna hua to 
+    if(size==n)
+    {
+        ListNode * newhead=head->next;
+        delete head;
+        return newhead;
+    }
+    int it=size-n;
+    while(it-1>0)
+    {
+        temp=temp->next;
+        it--;
+    }
+    // connection and deletion 
+    ListNode* del=temp->next;
+    temp->next=del->next;
+    delete del;
 
-   return head; 
+    return head;
+
+//    int size=0;
+//    ListNode *temp=head;
+//    if(head==NULL) return NULL;
+//    while(temp!=NULL)
+//    {
+//     size++;
+//     temp=temp->next;
+//    } 
+
+//    // if agr head hi remove krna pda to 
+//    if(n==size)
+//    {
+//     ListNode* newhead=head->next;
+//     delete head;
+//     return newhead;
+//    }
+//    int it=size-n;
+//    temp=head;
+//    for(int i=0;i<it-1;i++)
+//    {
+//     temp=temp->next;
+//    }
+//    ListNode* del=temp->next;
+//    temp->next=temp->next->next;
+//    delete del;
+
+//    return head; 
 }
 
 };
