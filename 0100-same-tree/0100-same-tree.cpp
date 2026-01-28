cb2e23587020e3b0c7b738b4f3087f160d6a3bcc
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-01-29 00:57:39 +0530

    Time: 0 ms (100%), Space: 12.7 MB (76.51%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,19 +14,15 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) 
     {
         if(p==NULL && q==NULL) return true;
-        if(p!=NULL &&q==NULL || p==NULL && q!=NULL) return false;
+        if(p==NULL && q!=NULL ||p!=NULL && q==NULL) return false;
+    
         if(p->val!=q->val) return false;
-
-        bool leftsubtree= isSameTree(p->left,q->left);
-        bool rightsubtree= isSameTree(p->right,q->right);
-
+        bool leftsubtree=isSameTree(p->left,q->left);
+        bool rightsubtree=isSameTree(p->right,q->right);
+        
         if(leftsubtree==false) return false;
         if(rightsubtree==false) return false;
 
-        
-        
-
         return true;
-        
     }
 };
