2c7cdcbfc84747fd0bc716ffbf4c1114876ae9a4
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-01-28 14:00:35 +0530

    Time: 0 ms (100%), Space: 32.2 MB (6.46%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -2,28 +2,23 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) 
     {
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        int n = nums.size();
-        int mncount = INT_MAX; // min length track karne ke liye
-
-        while (j < n)
+      int i=0;
+      int j=0;
+      int mncount=INT_MAX;
+      int sum=0;
+      int n=nums.size();
+      while(j<n)
+      {
+        sum+=nums[j];
+        while(sum>=target && i<=j)
         {
-            sum = sum + nums[j]; // nums[j] add karo
-
-            // Jab tak sum >= target ho, window shrink karte raho
-            while (i <= j && sum >= target)
-            {
-                mncount =min(mncount, j - i + 1); // min length update
-                sum = sum - nums[i];
-                i++;
-            }
-
-            j++; // window expand
+            mncount=min(mncount,j-i+1);
+            sum=sum-nums[i];
+            i++; // shirnk the i 
         }
-
-        if (mncount != INT_MAX) return mncount; // agar valid mila
-        return 0; // nahi mila to 0
+        j++;
+      }
+      if(mncount==INT_MAX) return 0;
+      else return mncount;  
     }
 };
