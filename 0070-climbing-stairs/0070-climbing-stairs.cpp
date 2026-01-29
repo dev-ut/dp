25166572b93fd3db290cad3e9c6e95e3413a83cc
Utkarsh Tiwari <utkarsht701@gmail.com>
2026-01-29 16:24:42 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.46%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    int solve(int indx, int n, vector<int>& memo) 
+// int solve(int n,int indx,vector<int>&memo)
+// {
+//     if(indx==n)
+//     {
+//         return 1;
+//     }
+//     if(indx>n) return 0;
+//     if(memo[indx]!=-1) return memo[indx];
+    
+//     int onestep=solve(n,indx+1,memo);
+//     int twostep=solve(n,indx+2,memo);
+//     memo[indx]=onestep+twostep;
+//     return memo[indx];
+
+// }
+    int climbStairs(int n) 
     {
-        if (indx == n) return 1;        // 1 valid path
-        if (indx > n) return 0;         // no valid path
+      
+    //   vector<int>memo(n+1,-1);
+     
+    //   return    solve(n,0,memo);  
 
-        if (memo[indx] != -1) return memo[indx];
-
-        // memoize the number of ways from current index
-        int onestep=solve(indx + 1, n, memo) ;
-        int twostep=solve(indx + 2, n, memo);
-
-        memo[indx]=onestep+twostep;
-        return memo[indx] ;
+    // atbulaton approch actual dp 
+    vector<int>dp(n+2,-1);
+    // abse cases
+    dp[n]=1;
+    dp[n-1]=1;
+    for(int i=n-2;i>=0;i--)
+    {
+        dp[i]=dp[i+1]+dp[i+2];
     }
+    return dp[0];
 
-    int climbStairs(int n) {
-        vector<int> memo(n + 1, -1);
-        return solve(0, n, memo);
     }
 };
-
-
